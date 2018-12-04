0caaa732ef74a593613c5596848cd01aec58a01d
Ariane Ancrenaz <ancrenaz.ariane@hotmail.fr>
2018-12-04 21:24:10 +0100

    implementation put et get

--- src/jus/proc/prodcons/v1/ProdConsBuffer.java
+++ src/jus/proc/prodcons/v1/ProdConsBuffer.java
@@ -17,23 +17,39 @@ public class ProdConsBuffer implements IProdConsBuffer{
 		taille = n;
 		l = new Lock();
 	}
+
+	public boolean estplein(){
+		return head==tail;
+	}
 	
 	public void put(Message m) throws InterruptedException {
+		while(l.tryLock()&&!estplein()){
+			try{wait();}
+			catch(InterruptedException e){continue;}
+		}
 		l.lock();
-	
-		
+		messages[tail] = m;
+		tail = (tail + 1)%taille;
 		l.unlock();
 	}
 	
 	public Message get() throws InterruptedException {
+		while(l.tryLock()){
+			try{wait();}
+			catch(InterruptedException e){continue;}
+		}
 		l.lock();
-
-
+		Message m = messages[head];
+		head = (head +1)%taille;
 		l.unlock();
-		return null;
+		return m;
 	}
 	
 	public int nmsg() {
+		while(l.tryLock()){
+			try{wait();}
+			catch(InterruptedException e){continue;}
+		}
 		l.lock();
 		
 		l.unlock();
